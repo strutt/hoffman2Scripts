f56fa0c45cbf2bd5d685efabaeb442b4413a5c23
Ben Strutt <b.strutt.12@ucl.ac.uk>
2017-04-26 17:03:08 -0700

    First attempt at a useful script

--- qsubCommand.sh
+++ qsubCommand.sh
@@ -1,1 +1,1 @@
-qsub -cwd -V -N test -l h_data=1024M,h_rt=00:10:00 -m n -t 1-10:1 /u/home/s/strutt/testQSub/test.sh
+qsub -cwd -V -N test -l h_data=4096M,h_rt=00:60:00 -m n -t 1300-4390:1 /u/home/s/strutt/hoffman2Scripts/test.sh
--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
-printenv
-/u/home/s/strutt/testQSub/test
+echo The GSE_TASK_ID is $SGE_TASK_ID
+outputDir=~/project-bern/$(date +%Y_%m_%d)
+mkdir -p $outputDir
+export OUTPUT_DIR=$outputDir
+
+$ANITA_UTIL_INSTALL_DIR/bin/Acclaim/drivers/filterDecimatedAcclaim
