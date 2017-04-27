29ab1c0fb492a765d38b5be3c481e512ee3519b7
Ben Strutt <strutt@physics.ucla.edu>
2017-04-26 17:36:51 -0700

    Maybe working job submission scripts

--- qsubCommand.sh
+++ qsubCommand.sh
@@ -1,1 +1,1 @@
-qsub -cwd -V -N test -l h_data=4096M,h_rt=00:60:00 -m n -t 1300-4390:1 /u/home/s/strutt/hoffman2Scripts/test.sh
+qsub -cwd -V -N $@ -l h_data=4096M,h_rt=00:60:00 -m n -t 1300-4390:1 /u/home/s/strutt/hoffman2Scripts/$@
