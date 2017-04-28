3ae6f7a541693b6e0cbcebf2ef260b8d8b816a4f
Ben Strutt <b.strutt.12@ucl.ac.uk>
2017-04-28 10:41:37 -0700

    Actual working version of hoffman2 submission scripts

--- test.sh
+++
@@ -1,6 +0,0 @@
-echo The GSE_TASK_ID is $SGE_TASK_ID
-outputDir=~/project-bern/$(date +%Y_%m_%d)
-mkdir -p $outputDir
-export OUTPUT_DIR=$outputDir
-
-$ANITA_UTIL_INSTALL_DIR/bin/Acclaim/drivers/filterDecimatedAcclaim
